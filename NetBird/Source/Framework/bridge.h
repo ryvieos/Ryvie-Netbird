13f8252c1149129f30a10d41e57d00e9c7f1b53e
maisonnavejul <jules.maisonnave@gmail.com>
2025-12-21 15:28:35 +0100

    Keep only ios/ folder, remove all other files

--- NetBird/Source/Framework/bridge.h
+++
@@ -1,9 +0,0 @@
-//
-//  bridge.m
-//  NetBirdiOS
-//
-//  Created by Pascal Fischer on 02.08.23.
-//
-
-#import <Foundation/Foundation.h>
-
--- NetBird/Source/Framework/ios-bridge.m
+++
@@ -1,8 +0,0 @@
-//
-//  ios-bridge.m
-//  NetBirdiOS
-//
-//  Created by Pascal Fischer on 02.08.23.
-//
-
-#import <Foundation/Foundation.h>
